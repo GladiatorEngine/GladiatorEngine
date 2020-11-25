ee625c9ed4a2873c46f7781d2eb25e7460b16f37
Pavel Kosilo <kodosgames@gmail.com>
2020-11-25 23:43:14 +0300

    Implement other GEA-asset binary format parts. There is still the question about hashing of AssetPack

--- Sources/AssetManagerC/AssetPack/GEAMAssetPackBuilder.m
+++ Sources/AssetManagerC/AssetPack/GEAMAssetPackBuilder.m
@@ -12,6 +12,11 @@
 - (id)initWithOutputPath:(NSString *)outputPath {
     self = [self init];
     packFile = fopen([outputPath cStringUsingEncoding:NSUTF8StringEncoding], "wb");
+    
+    // Write AssetPack byte
+    uint8_t assetTypeByte = AssetTypePack;
+    fwrite(&assetTypeByte, sizeof(uint8_t), 1, packFile);
+    
     return self;
 }
 
@@ -19,6 +24,15 @@
     NSData *data = [asset assetData];
     unsigned char *bytePtr = (unsigned char *)[data bytes];
     
+    // Write length
+    int64_t length = data.length;
+    fwrite(&length, sizeof(int64_t), 1, packFile);
+    
+    // Write AssetType
+    uint8_t assetTypeByte = [asset assetType];
+    fwrite(&assetTypeByte, sizeof(uint8_t), 1, packFile);
+    
+    // Write data itself
     fwrite(bytePtr, data.length, 1, packFile);
 }
 
